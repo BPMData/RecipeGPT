bdba23ec9cc9dfd68d5a0af7a329ea43683f4275
BPMData <123925276+BPMData@users.noreply.github.com>
2023-12-02 03:07:53 -0500

    Whoops, didn't commit this backend file.

---
+++ image_backend_2.py
@@ -0,0 +1,57 @@
+import os
+import requests
+import base64
+from PIL import Image
+from io import BytesIO
+import re
+
+def extract_title(recipe_text):
+    output = re.findall('^.*Recipe Title:.*$', recipe_text, re.MULTILINE)[0].strip().split('Recipe Title:')
+    title = output[1]
+    return title
+
+def get_stabilityai_image(recipe_title):
+    url = "https://api.stability.ai/v1/generation/stable-diffusion-xl-1024-v1-0/text-to-image"
+
+    # Updated request body with new parameters
+    body = {
+        "steps": 20,
+        "width": 1024,
+        "height": 1024,
+        "seed": 0,  # You can randomize this if you want different results each time
+        "cfg_scale": 5,
+        "samples": 1,
+        "style_preset": "photographic",
+        "text_prompts": [
+            {
+                "text": f"Crystal clear, high clarity award-winning cookbook cover photograph of a tantalizing dish of {recipe_title}, "
+                        f"alone on a rustic wooden table, close-up photograph, outdoors in Tuscany on a cloudy day.",
+                "weight": 1
+            },
+            {
+                "text": "Multiple dishes, empty table, zoomed out, blurry, out of focus",
+                "weight": -1
+            }
+        ],
+    }
+
+    # Updated headers with the API key
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {os.getenv('STABILITY_KEY')}",
+    }
+
+    # Check for non-200 response
+    response = requests.post(url, headers=headers, json=body)
+
+    if response.status_code != 200:
+        raise Exception("Non-200 response: " + str(response.text))
+
+    data = response.json()
+
+    # Assuming you want only the first generated image
+    image_data = data["artifacts"][0]["base64"]
+    image = Image.open(BytesIO(base64.b64decode(image_data)))
+
+    return image
