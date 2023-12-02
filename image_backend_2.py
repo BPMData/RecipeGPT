76ba8f7fc501d30a701438f7ecc0dfa688934871
BPMData <123925276+BPMData@users.noreply.github.com>
2023-12-02 03:14:58 -0500

    Turns out you can't go below 1024 with XL, which is weird but it also doesn't cost extra, so it's their GPU cycles I guess!

--- image_backend_2.py
+++ image_backend_2.py
@@ -10,6 +10,58 @@ def extract_title(recipe_text):
     title = output[1]
     return title
 
+"""Here's two styles of image generation - Barefoot Contessa, or modern social media. 
+Comment out the one you don't want to use."""
+
+# # Barefoot contessa style:
+# def get_stabilityai_image(recipe_title):
+#     url = "https://api.stability.ai/v1/generation/stable-diffusion-xl-1024-v1-0/text-to-image"
+#
+#     # Updated request body with new parameters
+#     body = {
+#         "steps": 20,
+#         "width": 512,
+#         "height": 512,
+#         "seed": 0,  # You can randomize this if you want different results each time
+#         "cfg_scale": 5,
+#         "samples": 1,
+#         "style_preset": "photographic",
+#         "text_prompts": [
+#             {
+#                 "text": f"Crystal clear, high clarity award-winning cookbook cover photograph of a tantalizing dish of {recipe_title}, "
+#                         f"alone on a rustic wooden table, close-up photograph, outdoors in Tuscany on a cloudy day.",
+#                 "weight": 1
+#             },
+#             {
+#                 "text": "Multiple dishes, empty table, zoomed out, blurry, out of focus",
+#                 "weight": -1
+#             }
+#         ],
+#     }
+#
+#     # Updated headers with the API key
+#     headers = {
+#         "Accept": "application/json",
+#         "Content-Type": "application/json",
+#         "Authorization": f"Bearer {os.getenv('STABILITY_KEY')}",
+#     }
+#
+#     # Check for non-200 response
+#     response = requests.post(url, headers=headers, json=body)
+#
+#     if response.status_code != 200:
+#         raise Exception("Non-200 response: " + str(response.text))
+#
+#     data = response.json()
+#
+#     # Assuming you want only the first generated image
+#     image_data = data["artifacts"][0]["base64"]
+#     image = Image.open(BytesIO(base64.b64decode(image_data)))
+#
+#     return image
+#
+
+# Modernist bon appetit style:
 def get_stabilityai_image(recipe_title):
     url = "https://api.stability.ai/v1/generation/stable-diffusion-xl-1024-v1-0/text-to-image"
 
@@ -24,8 +76,8 @@ def get_stabilityai_image(recipe_title):
         "style_preset": "photographic",
         "text_prompts": [
             {
-                "text": f"Crystal clear, high clarity award-winning cookbook cover photograph of a tantalizing dish of {recipe_title}, "
-                        f"alone on a rustic wooden table, close-up photograph, outdoors in Tuscany on a cloudy day.",
+                "text": f"High clarity, modernist cookbook cover photograph of {recipe_title}. Top-down photograph "
+                        f"showcasing the dish on a plate, placed on a plain metal background, highlighting the vibrant colors and textures of the food.",
                 "weight": 1
             },
             {
